e3b5c6ce50baed3cf4ef1c4af4413c5ea1921eba
Giulio Zausa <giulio.zausa@smc.it>
2020-03-14 15:34:28 +0100

    Sorted and fixed keys in flight

--- packages/common/src/Flight.ts
+++ packages/common/src/Flight.ts
@@ -1,21 +1,21 @@
 export default interface Flight {
-  YEAR: number; //2010
+  AIR_TIME: number; //90.00
+  ARR_DELAY: number; //-11.00
+  ARR_TIME: string; //"1749"
+  CANCELLED: number; //0.00
   DAY_OF_WEEK: number; //5
-  FL_DATE: Date; //2010-01-15
+  DEP_DELAY: number; //-5.00
+  DEP_TIME: string; //"1605"
+  DEST_AIRPORT_ID: number; //13244
+  DEST_CITY_NAME: string; //"Memphis, TN"
+  DEST_STATE_NM: string; //"Tennessee"
+  DEST: string; //"MEM"
+  FLIGHT_DATE: Date; //2010-01-15
   OP_CARRIER_AIRLINE_ID: number; //20363
   OP_CARRIER_FL_NUM: string; //"3692"
   ORIGIN_AIRPORT_ID: number; //14683
-  ORIGIN: string; //"SAT"
   ORIGIN_CITY_NAME: string; //"San Antonio, TX"
   ORIGIN_STATE_NM: string; //"Texas"
-  DEST_AIRPORT_ID: number; //13244
-  DEST: string; //"MEM"
-  DEST_CITY_NAME: string; //"Memphis, TN"
-  DEST_STATE_NM: string; //"Tennessee"
-  DEP_TIME: string; //"1605"
-  DEP_DELAY: number; //-5.00
-  ARR_TIME: string; //"1749"
-  ARR_DELAY: number; //-11.00
-  CANCELLED: number; //0.00
-  AIR_TIM: number; //90.00
+  ORIGIN: string; //"SAT"
+  YEAR: number; //2010
 }
