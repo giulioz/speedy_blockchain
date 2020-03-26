ad7a9ab7205e6d82d895aadf6c58ed8153f3cd53
Diletta Olliaro <dilettaolliaro@gmail.com>
2020-03-26 18:58:37 +0100

    convert date from string to timestamp before sending request

--- test_script/sendTrans.py
+++ test_script/sendTrans.py
@@ -1,6 +1,7 @@
 import json
 import requests
 import time
+import datetime
 
 jsonFile = 'dataFlights.json'
 api_endpoint = 'http://localhost:8080/transaction'
@@ -11,6 +12,11 @@ with open(jsonFile) as json_File:
 
 
 for record in jsonDecoded:
+	datestr = record['FL_DATE']
+	date = datetime.datetime.strptime(datestr, "%Y-%m-%d")
+	timestamp = datetime.datetime.timestamp(date)
+	record['FL_DATE'] = timestamp
+	print(record)
 	time.sleep(1)
 	r=requests.post(url=api_endpoint, data=record)
 	print(r.text)
