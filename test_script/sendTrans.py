cddb5b7818e1bd90889c835d27b88156561786d5
Giulio Zausa <giulio.zausa@smc.it>
2020-03-29 00:42:40 +0100

    Fixed endpoint in python script

--- test_script/sendTrans.py
+++ test_script/sendTrans.py
@@ -4,7 +4,7 @@ import time
 import datetime
 
 jsonFile = 'dataFlights.json'
-api_endpoint = 'http://localhost:8080/transaction'
+api_endpoint = 'http://localhost:8080/flight'
 
 
 with open(jsonFile) as json_File:
@@ -17,7 +17,7 @@ for record in jsonDecoded:
 	timestamp = datetime.datetime.timestamp(date)
 	record['FL_DATE'] = timestamp
 	print(record)
-	time.sleep(1)
+	time.sleep(0.1)
 	r=requests.post(url=api_endpoint, data=record)
 	print(r.text)
    	
