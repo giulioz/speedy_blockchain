7b7f3543d3ab0fd35b9ab0cfb4ae8d4cec09dd58
Diletta Olliaro <dilettaolliaro@gmail.com>
2020-03-29 12:26:59 +0200

    Fixed problem with missing values

--- test_script/sendTrans.py
+++ test_script/sendTrans.py
@@ -15,6 +15,7 @@ for record in jsonDecoded:
 	datestr = record['FL_DATE']
 	date = datetime.datetime.strptime(datestr, "%Y-%m-%d")
 	timestamp = datetime.datetime.timestamp(date)
+	print(record)
 	record['FL_DATE'] = timestamp
 	record['DAY_OF_WEEK'] = int(record['DAY_OF_WEEK'])
 	record['DEST_AIRPORT_ID'] = int(record['DEST_AIRPORT_ID'])
@@ -22,12 +23,12 @@ for record in jsonDecoded:
 	record['OP_CARRIER_FL_NUM'] = int(record['OP_CARRIER_FL_NUM'])
 	record['ORIGIN_AIRPORT_ID'] = int(record['ORIGIN_AIRPORT_ID'])
 	record['YEAR'] = int(record['YEAR'])
-	record['CANCELLED'] = bool(record['CANCELLED'])
-	if(record['CANCELLED']==False):
-		record['DEP_DELAY'] = float(record['DEP_DELAY'])
-		record['DEP_TIME'] = int(record['DEP_TIME'])
-		record['AIR_TIME'] = float(record['AIR_TIME'])
-		record['ARR_DELAY'] = float(record['ARR_DELAY'])
-		record['ARR_TIME'] = int(record['ARR_TIME'])
+	record['CANCELLED'] = float(record['CANCELLED'])
+	if record['DEP_DELAY']!= '' : record['DEP_DELAY'] = float(record['DEP_DELAY'])
+	if record['DEP_TIME']!= '' :	record['DEP_TIME'] = int(record['DEP_TIME'])
+	if record['AIR_TIME']!= '' :	record['AIR_TIME'] = float(record['AIR_TIME'])
+	if record['ARR_DELAY']!= '' :	record['ARR_DELAY'] = float(record['ARR_DELAY'])
+	if record['ARR_TIME']!= '' :	record['ARR_TIME'] = int(record['ARR_TIME'])
+	#print(record)	
 	time.sleep(1)
 	r=requests.post(url=api_endpoint, json=record)   	
