2e02c245b61c855afe3104e9da8daac68b74a117
Giulio Zausa <giulio.zausa@smc.it>
2020-03-29 13:53:37 +0200

    Lower difficulty

--- packages/common/src/Blockchain.ts
+++ packages/common/src/Blockchain.ts
@@ -6,7 +6,7 @@ import { genZeroes, getTimestamp } from "./utils";
 import AsyncMiner from "./AsyncMiner";
 
 // difficulty of our PoW algorithm
-export const DIFFICULTY = 4;
+export const DIFFICULTY = 2;
 
 // max transactions per block
 export const MAX_TRANSACTIONS = 4000;
