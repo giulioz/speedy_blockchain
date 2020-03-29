d54b820499b1d51c1b4f3e1d594ce8c1c5c9a54e
Giulio Zausa <giulio.zausa@smc.it>
2020-03-29 13:56:57 +0200

    Updated flight fields

--- packages/common/src/Flight.ts
+++ packages/common/src/Flight.ts
@@ -1,18 +1,18 @@
 export default interface Flight {
   AIR_TIME: number; // 90.00
   ARR_DELAY: number; // -11.00
-  ARR_TIME: string; // "1749"
+  ARR_TIME: number; // 1749
   CANCELLED: number; // 0.00
   DAY_OF_WEEK: number; // 5
   DEP_DELAY: number; // -5.00
-  DEP_TIME: string; // "1605"
+  DEP_TIME: number; // 1605
   DEST_AIRPORT_ID: number; // 13244
   DEST_CITY_NAME: string; // "Memphis, TN"
   DEST_STATE_NM: string; // "Tennessee"
   DEST: string; // "MEM"
-  FLIGHT_DATE: number; // 2010-01-15
+  FLIGHT_DATE: number; // 35865300
   OP_CARRIER_AIRLINE_ID: number; // 20363
-  OP_CARRIER_FL_NUM: string; // "3692"
+  OP_CARRIER_FL_NUM: number; // 3692
   ORIGIN_AIRPORT_ID: number; // 14683
   ORIGIN_CITY_NAME: string; // "San Antonio, TX"
   ORIGIN_STATE_NM: string; // "Texas"
